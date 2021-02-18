359953a26f8e22247c4b81119ed3cc089b0503e6
Виктория <tory_kuzya@mail.ru>
2021-02-18 15:41:03 +0300

    Реализовываем анимацию заголовков на экране «Результат игры»

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -3,7 +3,7 @@ export default () => {
   let results = document.querySelectorAll(`.screen--result`);
   if (results.length) {
     for (let i = 0; i < showResultEls.length; i++) {
-      showResultEls[i].addEventListener(`click`, function () {
+      showResultEls[i].addEventListener(`click`, function (e) {
         let target = showResultEls[i].getAttribute(`data-target`);
         [].slice.call(results).forEach(function (el) {
           el.classList.remove(`screen--show`);
@@ -14,6 +14,61 @@ export default () => {
         });
         targetEl[0].classList.add(`screen--show`);
         targetEl[0].classList.remove(`screen--hidden`);
+        if (targetEl[0].id === `result`) {
+          const paths = document.querySelectorAll(`.victory path`);
+          let pathLength;
+          for (let i = 0; i < paths.length; i++) {
+            pathLength = paths[i].getTotalLength();
+            const dasharray = `
+              <animate attributeName="stroke-dasharray"
+                                      dur="0.5s"
+                                      begin="titleResultOpacity.begin"
+                                      values="0 ${pathLength/3};
+                                              ${pathLength/3} 0"
+                                      fill="freeze"
+                                      repeatCount="1" />
+            `;
+            paths[i].innerHTML = dasharray;
+          }
+          const svg = document.querySelector(`#titleResultOpacity`);
+          svg.beginElement();
+        } else if (targetEl[0].id === `result2`) {
+          const paths = document.querySelectorAll(`.victory path`);
+          let pathLength;
+          for (let i = 0; i < paths.length; i++) {
+            pathLength = paths[i].getTotalLength();
+            const dasharray = `
+              <animate attributeName="stroke-dasharray"
+                                      dur="0.5s"
+                                      begin="titleResult2Opacity.begin"
+                                      values="0 ${pathLength/3};
+                                              ${pathLength/3} 0"
+                                      fill="freeze"
+                                      repeatCount="1" />
+            `;
+            paths[i].innerHTML = dasharray;
+          }
+          const svg = document.querySelector(`#titleResult2Opacity`);
+          svg.beginElement();
+        } else {
+          const paths = document.querySelectorAll(`.losing path`);
+          let pathLength;
+          for (let i = 0; i < paths.length; i++) {
+            pathLength = paths[i].getTotalLength();
+            const dasharray = `
+              <animate attributeName="stroke-dasharray"
+                                      dur="0.778s"
+                                      begin="titleResult3Opacity.begin"
+                                      values="0 ${pathLength/3};
+                                              ${pathLength/3} 0"
+                                      fill="freeze"
+                                      repeatCount="1" />
+            `;
+            paths[i].innerHTML = dasharray;
+          }
+          const svg = document.querySelector(`#titleResult3Opacity`);
+          svg.beginElement();
+        }
       });
     }
 
